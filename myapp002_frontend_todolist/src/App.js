b7b466ca6e6575b966593f547602834128c9b37c
EoooomJi <akcks101@gmail.com>
2023-01-31 21:44:24 +0900

    update

--- myapp002_frontend_todolist/src/App.js
+++ myapp002_frontend_todolist/src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const wrap = {
+    width: '500px',
+    border: '1px solid black',
+    margin: '10px auto',
+  };
+
+  let boardList = [
+    { id: 1, todoname: '운동하기', completed: 0 },
+    { id: 2, todoname: 'SNS꾸미기', completed: 0 },
+    { id: 3, todoname: '사진정리하기', completed: 0 },
+  ];
+
+  const [todos, setTodos] = useState([...boardList]);
+
+  const [input, setInput] = useState('');
+
+  const insertTodo = (e) => {
+    // form 형식의 기본 action은 '#'이다. 이벤트가 발생되지 않도록 preventDefault()를 작성한다.
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { id: todos.length + 1, todoname: input, completed: 0 },
+    ]);
+    setInput('');
+  };
+
+  const handleChangeText = (e) => {
+    setInput(e.target.value);
+  };
+
+  const updateTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, completed: todo.completed === 1 ? 0 : 1 }
+          : todo
+      )
+    );
+  };
+
+  useEffect(() => {
+    console.log('input:' + input);
+  }, [input]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={wrap}>
+      <h1>TODO LIST</h1>
+      <form onSubmit={insertTodo}>
+        <input
+          type='text'
+          required={true}
+          value={input}
+          onChange={handleChangeText}
+        />
+        <input type='submit' value='Create' />
+      </form>
+      {todos
+        ? todos.map((todo) => {
+            return (
+              <div className='todo' key={todo.id}>
+                <h3>
+                  <label
+                    className={todo.completed ? 'completed' : null}
+                    onClick={() => updateTodo(todo.id)}
+                  >
+                    {todo.todoname}
+                  </label>
+                </h3>
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 }
