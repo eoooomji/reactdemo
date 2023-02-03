119419bf917ab8a05488e1feddba419aa22c4cdb
EoooomJi <akcks101@gmail.com>
2023-02-03 21:45:21 +0900

    update

---
+++ myapp004_board/src/components/board/board_list.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import TableRow from './table_row';
+import axios from 'axios';
+import { baseUrl } from '../../commonAPI/todoApi';
+import PageNavigation from './page_nav';
+
+const BoardList = () => {
+  const [boardList, setBoardList] = useState([]);
+  const [pv, setPv] = useState({ currentPage: 1 });
+  const { currentPage } = useParams();
+
+  useEffect(() => {
+    getList(currentPage ? currentPage : 1);
+  }, []);
+
+  const getList = async (currentPage) => {
+    await axios
+      .get(baseUrl + '/board/list/' + currentPage)
+      .then((response) => {
+        //console.log(response.data);
+        setBoardList(response.data.aList);
+        setPv(response.data.pv);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <Link className='btn btn-danger' to='/board/write'>
+        글쓰기
+      </Link>
+      <h3 className='text-center'>게시판 목록</h3>
+      <table className='table table-striped ' style={{ marginTop: 20 }}>
+        <colgroup>
+          <col width='8%' />
+          <col width='*%' />
+          <col width='12%' />
+          <col width='12%' />
+        </colgroup>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>조회수</th>
+          </tr>
+        </thead>
+        <tbody>
+          {boardList.map((board) => {
+            return (
+              <TableRow
+                board={board}
+                key={board.num}
+                currentPage={currentPage}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+      <PageNavigation
+        currentPage={pv.currentPage}
+        startPage={pv.startPage}
+        endPage={pv.endPage}
+        blockPage={pv.blockPage}
+        totalPage={pv.totalPage}
+        getList={getList}
+      />
+    </div>
+  );
+};
+
+export default BoardList;
---
+++ myapp004_board/src/components/layout/BaseLayout.js
@@ -0,0 +1,34 @@
+import { NavLink, Outlet } from 'react-router-dom';
+
+function BaseLayout() {
+  const activeStyle = ({ isActive }) => ({
+    color: isActive ? 'green' : '',
+    fontsize: isActive ? '1.2rem' : '',
+  });
+
+  return (
+    <div>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <div className='container-fluid'>
+          <ul className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <NavLink style={activeStyle} className='nav-link' to='/'>
+                HOME
+              </NavLink>
+            </li>
+            <li className='nav-item'>
+              {/* NavLink, Link 내부에는 파라미터 값을 못 넘긴다. */}
+              <NavLink style={activeStyle} className='nav-link' to='/board'>
+                BOARD
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+export default BaseLayout;
